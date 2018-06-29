427171d15bd55dfafd98f1e849fc46ddaff7a3bb
jianan shang <18203355025@163.com>
2018-06-29 17:30:22 +0800

    实现了Tab5中的"增删改查"的功能，由于新增了ZengJiaBianGeng函数，更改了Update_Information_Jiedian函数，Tab4和Tab5的功能还需要进一步实验

--- WpfApp1/Tab5.cs
+++ WpfApp1/Tab5.cs
@@ -37,6 +37,63 @@ namespace WpfApp1
             Init_Jiedian_DisplayOrNot();//刷新所有节点
         }
 
+        private void ZengJiaBianGeng_Button_tab5_Click(object sender, RoutedEventArgs e)
+        {
+            ZengJiaBianGeng(Convert.ToInt16(BianHao_tab5.Text), JianCeQiTi_tab5.Text, AnZhuangWeiZhi_tab5.Text, AnZhuangShiJina_tab5.Text, GaoXianBaoJing_tab5.Text, DiXianBaoJing_tab5.Text);
+
+            //根据当前新增节点的坐标更新数据库中的数据
+            Init_Jiedian_DisplayOrNot();//刷新所有节点，包括地图模式和列表模式
+            //Update_Information_Jiedian(Ellipse_Array_tab4, Convert.ToInt16(BianHao.Text));
+        }
+
+        private void ShanChuDianWei_Button_tab5_Click(object sender, RoutedEventArgs e)
+        {
+            string command_str = "delete from table3_jiedian where id=" + BianHao_tab5.Text + ";";
+            MySqlHelper.GetDataSet("Database='" + ShuJuKu.ShuJuKu_Name + "';Data Source='localhost';User Id='root';Password='123456';charset='utf8';pooling=true",
+                                                  CommandType.Text, command_str, null);
+
+            Init_Jiedian_DisplayOrNot();//刷新所有节点，包括地图模式和列表模式
+        }
+
+        private void BianHao_tab5_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Show_Information_Jiedian_tab5(Convert.ToInt16(BianHao_tab5.Text));
+                jiedian_AutoMove_tab4(ref Ellipse_Array_tab4, Convert.ToInt16(BianHao_tab5.Text) - 1);
+                jiedian_AutoZoom_tab4(ref Ellipse_Array_tab4, Convert.ToInt16(BianHao_tab5.Text) - 1);
+            }
+        }
+
+        /// <summary>
+        /// 显示编号，检测气体， 安装位置，安装时间，高限报警，低限报警
+        /// </summary>
+        public void Show_Information_Jiedian_tab5(int index)
+        {
+            DataSet dataSet_temp = new DataSet();
+            string command_str = "select * from " + ShuJuKu.Table3_JieDian + " where `id`=" + index.ToString();
+            dataSet_temp = MySqlHelper.GetDataSet("Database='" + ShuJuKu.ShuJuKu_Name + "';Data Source='localhost';User Id='root';Password='123456';charset='utf8';pooling=true",
+                                                  CommandType.Text, command_str, null);
+            DataRowCollection temp_DataRow = dataSet_temp.Tables[0].Rows;//获取列
+
+            if (temp_DataRow.Count <= 0)//没有相应节点的信息，就将下面的数据设置成默认值
+            {
+                JianCeQiTi_tab5.Text = "默认值";
+                AnZhuangWeiZhi_tab5.Text = "默认值";
+                AnZhuangShiJina_tab5.Text = DateTime.Now.ToString();//默认为当前时间
+                GaoXianBaoJing_tab5.Text = "默认值";
+                DiXianBaoJing_tab5.Text = "默认值";
+            }
+            else
+            {
+                JianCeQiTi_tab5.Text = temp_DataRow[0][1].ToString();
+                AnZhuangWeiZhi_tab5.Text = temp_DataRow[0][2].ToString();
+                AnZhuangShiJina_tab5.Text = temp_DataRow[0][3].ToString();
+                GaoXianBaoJing_tab5.Text = temp_DataRow[0][4].ToString();
+                DiXianBaoJing_tab5.Text = temp_DataRow[0][5].ToString();
+            }
+        }
+
         private void DiTuMoShi_Tab5_Buttton_Click(object sender, RoutedEventArgs e)
         {
             tabcontrol.SelectedIndex = 4;
