7d11a88f96c61446e8d7b7388a087b80453a1db2
jianan shang <18203355025@163.com>
2018-07-11 11:39:51 +0800

    某些节点的坐标更改不正确，已修正 更改不正确的原因是某些列表的索引是从零开始，有些是从一

--- WpfApp1/Tab4.cs
+++ WpfApp1/Tab4.cs
@@ -316,8 +316,8 @@ namespace WpfApp1
             List<int> ids_list = get_exit_jiedian_id_list();
             foreach (int mem in ids_list)
             {
-                Point point_temp = Ellipse_Array_tab4[mem].TranslatePoint(new Point(0, 0), img_tab4);
-                string command_str = "update " + ShuJuKu.Table3_JieDian + " SET xmin = '" + point_temp.X.ToString() + "', ymin = '" + point_temp.Y.ToString() + "' WHERE id = " + (mem + 1).ToString() + ";";
+                Point point_temp = Ellipse_Array_tab4[(mem - 1)].TranslatePoint(new Point(0, 0), img_tab4);
+                string command_str = "update " + ShuJuKu.Table3_JieDian + " SET xmin = '" + point_temp.X.ToString() + "', ymin = '" + point_temp.Y.ToString() + "' WHERE id = " + (mem).ToString() + ";";
                 MySqlHelper.GetDataSet("Database='" + ShuJuKu.ShuJuKu_Name + "';Data Source='localhost';User Id='root';Password='123456';charset='utf8';pooling=true", CommandType.Text, command_str, null);
             }
             Init_Jiedian_DisplayOrNot();//刷新所有节点
